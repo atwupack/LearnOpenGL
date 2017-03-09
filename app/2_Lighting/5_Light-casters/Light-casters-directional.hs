0573a293ccdc17839bbff1344b7dc9551365f7a0
André Twupack <atwupack@mailbox.org>
2017-03-09 21:42:53 +0100

    add directional light tutorial

--- app/2_Lighting/5_Light-casters/Light-casters-directional.hs
+++ app/2_Lighting/5_Light-casters/Light-casters-directional.hs
@@ -21,6 +21,19 @@ import Reactive.Banana.Combinators hiding (empty)
 import LOGL.FRP
 import LOGL.Objects
 
+cubePositions :: [V3 GLfloat]
+cubePositions = [
+    V3 0.0  0.0  0.0,
+    V3 2.0  5.0 (-15.0),
+    V3 (-1.5) (-2.2) (-2.5),
+    V3 (-3.8) (-2.0) (-12.3),
+    V3 2.4 (-0.4) (-3.5),
+    V3 (-1.7) 3.0 (-7.5),
+    V3 1.3 (-2.0) (-2.5),
+    V3 1.5  2.0 (-2.5),
+    V3 1.5  0.2 (-1.5),
+    V3 (-1.3)  1.0 (-1.5)]
+
 main :: IO ()
 main = do
     GLFW.init
@@ -33,8 +46,8 @@ main = do
     lampShader <- simpleShaderProgram ("data" </> "2_Lighting" </> "1_Colors" </> "lamp.vs")
         ("data" </> "2_Lighting" </> "1_Colors" </> "lamp.frag")
 
-    lightingShader <- simpleShaderProgram ("data" </> "2_Lighting" </> "4_Lighting-maps" </> "specular.vs")
-        ("data" </> "2_Lighting" </> "4_Lighting-maps" </> "specular.frag")
+    lightingShader <- simpleShaderProgram ("data" </> "2_Lighting" </> "5_Light-casters" </> "directional.vs")
+        ("data" </> "2_Lighting" </> "5_Light-casters" </> "directional.frag")
 
     diffuseMap <- createTexture ("data" </> "2_Lighting" </> "4_Lighting-maps" </> "container2.png")
     specularMap <- createTexture ("data" </> "2_Lighting" </> "4_Lighting-maps" </> "container2_specular.png")
@@ -76,38 +89,44 @@ drawScene lightingShader contVAO lampShader lightVAO w cam = do
     -- draw the container cube
     currentProgram $= Just (program lightingShader)
 
-    let lightX = 1.0 + 2.0 * sin time
-        lightY = sin (time / 2.0)
-        lightPos = V3 (realToFrac  lightX) (realToFrac lightY) (2.0 :: GLfloat)
+    --let lightX = 1.0 + 2.0 * sin time
+        --lightY = sin (time / 2.0)
+        --lightPos = V3 (realToFrac  lightX) (realToFrac lightY) (2.0 :: GLfloat)
 
-    setUniform lightingShader "light.position" lightPos
+    setUniform lightingShader "light.direction" (V3 (-0.2 :: GLfloat) (-1.0) (-0.3))
     setUniform lightingShader "viewPos" (position cam)
 
     setUniform lightingShader "light.ambient" (V3 (0.2 :: GLfloat) 0.2 0.2)
     setUniform lightingShader "light.diffuse" (V3 (0.5 :: GLfloat) 0.5 0.5)
     setUniform lightingShader "light.specular" (V3 (1.0 :: GLfloat) 1.0 1.0)
 
-    setUniform lightingShader "material.shininess" (64.0 :: GLfloat)
+    setUniform lightingShader "material.shininess" (32.0 :: GLfloat)
 
     let view = viewMatrix cam
         projection = perspective (radians (zoom cam)) (800.0 / 600.0) 0.1 (100.0 :: GLfloat)
     setUniform lightingShader "view" view
     setUniform lightingShader "projection" projection
 
-    let model = identity :: M44 GLfloat
-    setUniform lightingShader "model" model
-    withVAO contVAO $ drawArrays Triangles 0 36
+    withVAO contVAO $ mapM_ (drawCube lightingShader) [0..9]
 
     -- draw the lamp
-    currentProgram $= Just (program lampShader)
-    setUniform lampShader "view" view
-    setUniform lampShader "projection" projection
+    -- currentProgram $= Just (program lampShader)
+    -- setUniform lampShader "view" view
+    -- setUniform lampShader "projection" projection
 
-    let model = mkTransformationMat (0.2 *!! identity) lightPos
-    setUniform lampShader "model" model
-    withVAO lightVAO $ drawArrays Triangles 0 36
+    -- let model = mkTransformationMat (0.2 *!! identity) lightPos
+    -- setUniform lampShader "model" model
+    -- withVAO lightVAO $ drawArrays Triangles 0 36
 
     swap w
+
+drawCube :: ShaderProgram -> Int -> IO ()
+drawCube shader i = do
+    let angle = pi / 180.0 * 20.0 * fromIntegral i
+        rot = axisAngle (V3 (1.0 :: GLfloat) 0.3 0.5) (realToFrac angle)
+        model = mkTransformation rot (cubePositions !! i)
+    setUniform shader "model" model
+    drawArrays Triangles 0 36
 
 createCubeVBO :: IO BufferObject
 createCubeVBO = makeBuffer ArrayBuffer cubeWithNormalsAndTexture
