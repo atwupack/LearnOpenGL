27ac7caab41e8a8a3fec3f6033509ded6a02aeb6
André Twupack <atwupack@mailbox.org>
2017-03-13 15:49:34 +0100

    first mesh implementation complete

--- src/LOGL/Mesh.hs
+++ src/LOGL/Mesh.hs
@@ -10,8 +10,11 @@ import Graphics.Rendering.OpenGL.GL as GL hiding (Vertex, normalize, position)
 import Foreign.Storable
 import Foreign.Ptr
 import Graphics.GLUtil.BufferObjects
+import Graphics.GLUtil.ShaderProgram
+import Graphics.GLUtil.VertexArrayObjects
 
 data Vertex = Vertex { position :: V3 GLfloat, normal :: V3 GLfloat, texCoords :: V2 GLfloat }
+    deriving (Eq, Show)
 
 instance Storable Vertex where
     sizeOf _ = 2 * sizeOf (undefined :: V3 GLfloat) + sizeOf (undefined :: V2 GLfloat)
@@ -26,20 +29,37 @@ instance Storable Vertex where
         where
             ptr' = castPtr ptr
 
-data Texture = Texture { tobj :: TextureObject, ttype :: TextureType }
+data Texture = Texture { tobj :: TextureObject, ttype :: TextureType, tname :: String }
+    deriving (Eq, Show)
 
 data TextureType = DiffuseMap | SpecularMap | NormalMap
+    deriving (Eq, Show)
 
-data Mesh = Mesh { vao :: VertexArrayObject, vbo :: BufferObject, ebo :: BufferObject}
+data Mesh = Mesh { vertices :: [Vertex], indices :: [GLuint], textures :: [Texture], vao :: VertexArrayObject, vbo :: BufferObject, ebo :: BufferObject}
+    deriving (Eq, Show)
+
+draw :: Mesh -> ShaderProgram -> IO ()
+draw mesh shader = do
+    mapM_ (setTexture shader mesh)  [0..(fromIntegral (length (textures mesh)))]
+    withVAO (vao mesh) $ drawElements Triangles (fromIntegral (length (indices mesh))) UnsignedInt nullPtr
+
+setTexture :: ShaderProgram -> Mesh -> GLuint -> IO ()
+setTexture shader mesh tu = do
+    let text = (textures mesh) !! fromIntegral tu
+        name = "material.texture_" ++ (tname text)
+    activeTexture $= TextureUnit tu
+    textureBinding Texture2D $= Just (tobj text)
+    setUniform shader name (TextureUnit tu)
+
 
 createMesh :: [Vertex] -> [GLuint] -> [Texture] -> IO Mesh
-createMesh verts indices texts = do
-    vao <- genObjectName
-    vbo <- makeBuffer ArrayBuffer verts
-    ebo <- makeBuffer ElementArrayBuffer indices
+createMesh verts inds texts = do
+    newVao <- genObjectName
+    newVbo <- makeBuffer ArrayBuffer verts
+    newEbo <- makeBuffer ElementArrayBuffer inds
 
-    bindVertexArrayObject $= Just vao
-    bindBuffer ArrayBuffer $= Just vbo
+    bindVertexArrayObject $= Just newVao
+    bindBuffer ArrayBuffer $= Just newVbo
     vertexAttribPointer (AttribLocation 0) $= (ToFloat, VertexArrayDescriptor 3 Float (8*4) offset0)
     vertexAttribArray (AttribLocation 0) $= Enabled
     vertexAttribPointer (AttribLocation 1) $= (ToFloat, VertexArrayDescriptor 3 Float (8*4) (offsetPtr (3*4)))
@@ -47,4 +67,5 @@ createMesh verts indices texts = do
     vertexAttribPointer (AttribLocation 2) $= (ToFloat, VertexArrayDescriptor 2 Float (8*4) (offsetPtr (6*4)))
     vertexAttribArray (AttribLocation 2) $= Enabled
     bindVertexArrayObject $= Nothing
-    return $ Mesh vao vbo ebo
+    return Mesh {
+        vertices = verts, indices = inds, textures = texts, vao = newVao, vbo = newVbo, ebo = newEbo }
